55c175d360718b5c63053f3524b4603de8b5b540
Snickode7 <nickmath21@gmail.com>
2018-05-17 18:17:12 -0400

    Delete add-activity.component.ts

--- ClientApp/src/app/components/add-activity/add-activity.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-add-activity',
-  templateUrl: './add-activity.component.html',
-})
-export class AddActivityComponent {
-}
