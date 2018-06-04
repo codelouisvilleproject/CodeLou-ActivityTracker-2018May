d7c0d90c8e5af8663caaa751f86eea2a3f2a6349
Chris Akridge <cakridge2@gmail.com>
2018-06-04 08:17:52 -0400

    Attempt to seed database

--- Database/ActivityContext.cs
+++ Database/ActivityContext.cs
@@ -1,6 +1,7 @@
 using ActivityTracker.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ActivityTracker.Database
 {
@@ -16,6 +17,16 @@ namespace ActivityTracker.Database
         {
             // Use SQLite for Dev, will use something else for production. SQLite is GREAT for local development if you can get away with it
             optionsBuilder.UseSqlite("Data Source=activity.db");
+
+            // Really weird way to seed the DB since DB seeding as an EF builtin only exists in 2.1, and we're on 2.0.
+            if (!Activities.Any())
+            {
+                Activities.Add(new Activity { Id = 1, Name = "Running", MeasurementType = ActivityMeasurementType.LengthOfTime });
+                Activities.Add(new Activity { Id = 2, Name = "Stretching", MeasurementType = ActivityMeasurementType.LengthOfTime });
+                Activities.Add(new Activity { Id = 3, Name = "Weightlifting", MeasurementType = ActivityMeasurementType.RepsWithWeight });
+                Activities.Add(new Activity { Id = 4, Name = "Swimming", MeasurementType = ActivityMeasurementType.LengthOfTime });
+                Activities.Add(new Activity { Id = 5, Name = "Jumping Jacks", MeasurementType = ActivityMeasurementType.Reps });
+            }
         }
     }
 }
