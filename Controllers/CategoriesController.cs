94de8cc518985de36445110440486e2dc3d458b3
Gavin Schilling <schillgc@gmail.com>
2018-06-04 14:40:34 -0400

    Add additional functionality

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -2,40 +2,58 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using ActivityTracker.Models;
+using ActivityTracker.Database;
 
 namespace ActivityTracker.Controllers
 {
     [Route("api/[controller]")]
     public class CategoriesController : ControllerBase
     {
-        private readonly CategoriesContext _context;
-
-        public CategoriesController(CategoriesContext context)
+        public CategoriesController()
         {
-            _context = context;
-
-            if (_context.CategoriesItems.Count() == 0)
+            using (var context = new ActivityContext())
             {
-                _context.CategoriesItems.Add(new CategoriesItem { Name = "Item1" });
-                _context.SaveChanges();
+                if(context.Categories.Where( x => x.Name  == "Cardio" ).Count() == 0)
+                {
+                    context.Categories.Add(new Categories { Name = "Cardio" });
+                    context.SaveChanges();
+                };
+
+                if(context.Categories.Where( x => x.Name  == "Flexibility" ).Count() == 0)
+                {
+                    context.Categories.Add(new Categories { Name = "Flexibility" });
+                    context.SaveChanges();     
+                };
+
+                if(context.Categories.Where( x => x.Name  == "CardioStrength" ).Count() == 0)
+                {
+                        context.Categories.Add(new Categories { Name = "CardioStrength" });
+                        context.SaveChanges();
+                };
             }
         }
 
         [HttpGet]
-        public List<CategoriesItem> GetAll()
+        public List<Categories> GetAll()
         {
-            return _context.CategoriesItems.ToList();
+            using (var context = new ActivityContext())
+            {
+                return context.Categories.ToList();
+            }
         }
 
         [HttpGet("{id}", Name = "GetCategory")]
         public IActionResult GetById(long id)
         {
-            var item = _context.CategoriesItems.Find(id);
-            if (item == null)
+            using (var context = new ActivityContext())
             {
-                return NotFound();
+                var item = context.Categories.Find(id);
+                if (item == null)
+                {
+                    return NotFound();
+                }
+                return Ok(item);
             }
-            return Ok(item);
-        }       
+        }
     }
 }
